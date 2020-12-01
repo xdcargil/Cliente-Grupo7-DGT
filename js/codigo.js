fb34e30157c2676142a391f61c0e4a6ae7e48efb
xdaraboz <73521177+xdaraboz@users.noreply.github.com>
2020-12-01 13:47:57 +0100

    Limpiar modales
    ya se limpian al cerrar

--- js/codigo.js
+++ js/codigo.js
@@ -186,6 +186,17 @@ function btnGravePulsado() {
 
 }
 
+//Limpia los modales cuando se clickea en cerrar de cualquier modal
+function limpiarModal(){
+  let inputs=document.getElementsByTagName('input');
+  for (let index = 0; index < inputs.length; index++) {
+      if (inputs[index].type=="text") {
+          inputs[index].value="";
+      }
+      
+  }
+}
+
 
 
 /*Listado de Conductores*/
