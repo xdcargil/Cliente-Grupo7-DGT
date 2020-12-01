ab25d44c7d7a9273762e8d9da6316b366d602e7c
xdaraboz <73521177+xdaraboz@users.noreply.github.com>
2020-12-01 13:23:58 +0100

    altas validadas a medias
    y display cambiado al html de  los botones leve y grave

--- js/codigo.js
+++ js/codigo.js
@@ -3,13 +3,13 @@ var oDGT = new DGT();
 
 function altaConductor() {
 
-    let sNif = parseInt(frmAltaConductor.txtNIF.value.trim());
+    let sNif =frmAltaConductor.txtNIF.value.trim();
     let sNombre = frmAltaConductor.txtNombre.value.trim();
     let sApellido = frmAltaConductor.txtApellidos.value.trim();
     let sDireccion = frmAltaConductor.txtDireccion.value.trim();
     let dFechaCarnet = new Date(frmAltaConductor.dFechaCarnet.value);
 
-    if (sNif && sNombre && sApellido && sDireccion != NaN && sNif && sNombre && sApellido && sDireccion.length > 0) {
+    if (sNif && sNombre && sApellido && sDireccion.length && dFechaCarnet !="") {
 
         let oNuevoConductor = new Conductor(sNif, sNombre, sApellido, sDireccion, dFechaCarnet);
         if (oDGT.altaConductor(oNuevoConductor)) {
@@ -30,33 +30,38 @@ function altaConductor() {
 
 function altaGuardiaCivil() {
 
-    let sNif = parseInt(frmAltaGuardiaCivil.txtNIF.value.trim());
+    let sNif = frmAltaGuardiaCivil.txtNIF.value.trim();
     let sNombre = frmAltaGuardiaCivil.txtNombre.value.trim();
     let sApellido = frmAltaGuardiaCivil.txtApellidos.value.trim();
     let sDireccion = frmAltaGuardiaCivil.txtDireccion.value.trim();
     let sPuesto = frmAltaGuardiaCivil.txtPuesto.value.trim();
 
+    if (sNif && sNombre && sApellido && sDireccion  && sPuesto !="") {
 
     let oNuevoGuardiaCivil = new GuardiaCivil(sNif, sNombre, sApellido, sDireccion, sPuesto);
 
     if (oDGT.altaGuardiaCivil(oNuevoGuardiaCivil)) {
         alert("Guardia Agregado");
+        $('#altaGuardiaCivilModal').modal('hide'); //Esta función cierra el modal.
     } else {
         alert("El guardia no se ha podido agregar");
     }
 
-    $('#altaGuardiaCivilModal').modal('hide'); //Esta función cierra el modal.
+    
+}else{
+    alert("rellene todos los campos");
 }
 
 function registrarMulta() {
     let sIdMulta = parseInt(frmRegistroMulta.txtID.value.trim());
-    let sNifConductor = parseInt(frmRegistroMulta.txtNIFConductor.value.trim());
-    let sNifGuardia = parseInt(frmRegistroMulta.txtNIFGuardiaCivil.value.trim());
+    let sNifConductor =frmRegistroMulta.txtNIFConductor.value.trim();
+    let sNifGuardia = frmRegistroMulta.txtNIFGuardiaCivil.value.trim();
     let fImporte = parseFloat(frmRegistroMulta.txtImporte.value.trim());
     let sDescripcion = frmRegistroMulta.txtDescripcion.value.trim();
     let dFechaMulta = new Date(frmRegistroMulta.txtFechaAltaMulta.value);
 
     if (oDGT._buscarConductor(sNifConductor) != null && oDGT._buscarGuardia(sNifGuardia) != null) {
+        if((sIdMulta && fImporte && sDescripcion /* && dFechaMulta*/ !="" )){
         if (frmRegistroMulta.txtPuntos.value.trim().length > 0) {
             //alta grave
             let iPuntos = parseInt(frmRegistroMulta.txtPuntos.value.trim());
@@ -78,8 +83,12 @@ function registrarMulta() {
 
             oDGT.registrarMulta(oLeve) ? alert("Se ha registrado la multa") : alert("No se ha podido registrar la multa");
         }
+    }
+    else{
+        alert("Rellene todos los campos");
+    }
     } else {
-        alert("Error al validar algún NIF");
+        alert("Error al validar NIF");
     }
 
 
@@ -149,12 +158,6 @@ function imprimirMulta() {
 
 /* MANIPULAR UI */
 
-//Carga los styles cuadno carga la ventana para que aparezcan escondidos los containers
-//de los botones leve y grave
-/*window.onload = function () {
-    document.getElementById("multiCollapseRadios").style.display = "none";
-    document.getElementById("multiCollapsePuntos").style.display = "none";
-};
 
 //Ocultamos los botones cuando puslsamos el boton de registrar multa
 function ocultarLeveYGrave() {
@@ -178,7 +181,7 @@ function btnGravePulsado() {
     document.getElementById("multiCollapseRadios").style.display = "none";
     document.getElementById("multiCollapsePuntos").style.display = "block";
 
-}*/
+}
 
 
 
