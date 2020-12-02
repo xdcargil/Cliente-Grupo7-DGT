32074c3b5c5c0bde5d25a3351ff40fe57a515546
Daniel <xdcargil021@ieshnosmachado.org>
2020-12-02 19:39:36 +0100

    update clases

--- js/objetos.js
+++ js/objetos.js
@@ -1,27 +1,25 @@
 "use strict";
 
 // ------------- Clase Persona  -------------
-class Persona {
-
-    constructor(sNif, sNombre, sApellidos, sDireccion) {
-        this.NIF = sNif;
-        this.nombre = sNombre;
-        this.apellidos = sApellidos;
-        this.direccion = sDireccion;
-    }
-
-    toHTMLRow() {
-        let sFila = "<tr>";
-        sFila += "<td>" + this.NIF + "</td>";
-        sFila += "<td>" + this.nombre + "</td>";
-        sFila += "<td>" + this.apellidos + "</td>";
-        sFila += "<td>" + this.direccion + "</td>";
-        sFila += "</tr>";
-
-        return sFila;
-    }
-
+function Persona(sNif, sNombre, sApellidos, sDireccion) {
+    this.NIF = sNif;
+    this.nombre = sNombre;
+    this.apellidos = sApellidos;
+    this.direccion = sDireccion;
 }
+
+Persona.prototype.toHTMLRow = function() {
+    let sFila = "<tr>";
+    sFila += "<td>" + this.NIF + "</td>";
+    sFila += "<td>" + this.nombre + "</td>";
+    sFila += "<td>" + this.apellidos + "</td>";
+    sFila += "<td>" + this.direccion + "</td>";
+    sFila += "</tr>";
+
+    return sFila;
+}
+
+
 // ------------- FIN Clase Persona  -------------
 
 
@@ -133,18 +131,18 @@ class DGT {
         let resultadoConductor = "";
         aConductores.forEach(conductor => {
             aMultasGraves.forEach(multa => {
-                if(conductor.NIF == multa.NIFConductor){
+                if (conductor.NIF == multa.NIFConductor) {
                     resultadoConductor += "<tr>";
                     resultadoConductor += "<td>" + conductor.NIF + "</td>";
                     resultadoConductor += "<td>" + multa.puntos + "</td>";
                     resultadoConductor += "</tr>";
-            
+
                 }
             });
         });
-        if(resultadoConductor.length>0){
+        if (resultadoConductor.length > 0) {
             return resultadoConductor;
-        }else{
+        } else {
             return 1; //1 es igual a no hay multas
         }
 
@@ -169,7 +167,7 @@ class DGT {
 
             for (let oP of oConductorAux) {
                 sTabla += oP.toHTMLRow();
-              
+
             }
             sTabla += "</tbody>";
 
@@ -212,7 +210,7 @@ class DGT {
 
     listadoMultasPorFecha(dFechaIni, dFechaFin) {
         let oListaMultas = this._multas.filter(oMul => oMul.fecha.getTime() >= dFechaIni.getTime() && oMul.fecha.getTime() <= dFechaFin.getTime());
-        let sTabla="";
+        let sTabla = "";
 
         if (oListaMultas.length > 0) {
 
@@ -244,7 +242,7 @@ class DGT {
         return sTabla;
     }
 
-    listarMultasPorGuardia(){
+    listarMultasPorGuardia() {
 
         let sTabla = '<table border="1">';
         // Encabezado de la tabla
@@ -254,32 +252,32 @@ class DGT {
         sTabla += "<th>Rango</th>";
         sTabla += "<th>Número de multas</th>";
         sTabla += "</tr></thead>";
-        
+
         let oGuardiaAux = this._personas.filter(persona => persona instanceof GuardiaCivil);
-        let contadorMultas=0;
-        
+        let contadorMultas = 0;
+
         //Agrega un atributo a cada guardia con un numero de multas
         for (let oG of oGuardiaAux) {
-            oG.numeroMultas=0;
+            oG.numeroMultas = 0;
         }
 
         //Mira si la array tiene datos
-        if(oGuardiaAux.length > 0){
-            
+        if (oGuardiaAux.length > 0) {
+
             //Recorre la array de guardias
             for (let oP of oGuardiaAux) {
-               /*  sTabla += oP.toHTMLRow(); */
-               //Para cada nuevo guardia le pones el contador a 0
-                contadorMultas=0;
+                /*  sTabla += oP.toHTMLRow(); */
+                //Para cada nuevo guardia le pones el contador a 0
+                contadorMultas = 0;
                 //Recorre la array multas y mira si el nif del guardia esta en el nifGuardia de la multa
-                for(let oM of this._multas){
-                    if(oP.NIF == oM.NIFGuardia){
+                for (let oM of this._multas) {
+                    if (oP.NIF == oM.NIFGuardia) {
                         contadorMultas++;
                         console.log(contadorMultas);
                     }
                 }
                 //Agrega al atributo numeroMultas el contador
-                oP.numeroMultas=contadorMultas;
+                oP.numeroMultas = contadorMultas;
 
                 let sFila = "<tr>";
                 sFila += "<td>" + oP.NIF + "</td>";
@@ -287,19 +285,19 @@ class DGT {
                 sFila += "<td>" + oP.apellidos + "</td>";
                 sFila += "<td>" + oP.direccion + "</td>";
                 sFila += "<td>" + oP.puesto + "</td>";
-                sFila += "<td>" + oP.numeroMultas + "</td>";     
+                sFila += "<td>" + oP.numeroMultas + "</td>";
                 sFila += "</tr>";
-                sTabla+=sFila;
+                sTabla += sFila;
 
             }
             console.log(oGuardiaAux);
             sTabla += "</tbody>";
-            
-             return sTabla;
+
+            return sTabla;
         }
-        else{
-            
-           return "No hay Guardia civil";
+        else {
+
+            return "No hay Guardia civil";
         }
     }
 
@@ -308,26 +306,23 @@ class DGT {
 // ------------- FIN Clase DGT   -------------
 
 // ------------- Clase Conductor hereda de PERSONA  -------------
-class Conductor extends Persona { //Clase Conductor hereda de Persona
-    constructor(sNif, sNombre, sApellidos, sDireccion, dCaducidadCarnet) {
-        super(sNif, sNombre, sApellidos, sDireccion);
+function Conductor(sNif, sNombre, sApellidos, sDireccion, dCaducidadCarnet)  { //Clase Conductor hereda de Persona
+        Persona.call(this, sNif, sNombre, sApellidos, sDireccion);
         this.caducidadCarnet = dCaducidadCarnet;
-    }
 
-    toHTMLRow() {
-        let sFila = "<tr>";
-        sFila += "<td>" + this.NIF + "</td>";
-        sFila += "<td>" + this.nombre + "</td>";
-        sFila += "<td>" + this.apellidos + "</td>";
-        sFila += "<td>" + this.direccion + "</td>";
-        sFila += "<td>" + this.caducidadCarnet + "</td>";
+}
 
-        sFila += "</tr>";
+Conductor.prototype.toHTMLRow = function () {
+    let sFila = "<tr>";
+    sFila += "<td>" + this.NIF + "</td>";
+    sFila += "<td>" + this.nombre + "</td>";
+    sFila += "<td>" + this.apellidos + "</td>";
+    sFila += "<td>" + this.direccion + "</td>";
+    sFila += "<td>" + this.caducidadCarnet + "</td>";
 
-        return sFila;
-    }
+    sFila += "</tr>";
 
-
+    return sFila;
 }
 // ------------- FIN Clase Conductor hereda de PERSONA  -------------
 
@@ -335,126 +330,125 @@ class Conductor extends Persona { //Clase Conductor hereda de Persona
 
 
 // ------------- Clase GuardiaCivil hereda de PERSONA  -------------
-class GuardiaCivil extends Persona { //Clase GuardiaCivil hereda de Persona
-    constructor(sNif, sNombre, sApellidos, sDireccion, sPuesto) {
-        super(sNif, sNombre, sApellidos, sDireccion);
+function  GuardiaCivil(sNif, sNombre, sApellidos, sDireccion, sPuesto) { //Clase GuardiaCivil hereda de Persona
+        Persona.call(this,sNif, sNombre, sApellidos, sDireccion);
         this.puesto = sPuesto;
+    
+}
+
+GuardiaCivil.prototype.toHTMLRow = function() {
+    let sFila = "<tr>";
+    sFila += "<td>" + this.NIF + "</td>";
+    sFila += "<td>" + this.nombre + "</td>";
+    sFila += "<td>" + this.apellidos + "</td>";
+    sFila += "<td>" + this.direccion + "</td>";
+    sFila += "<td>" + this.puesto + "</td>";
+
+    sFila += "</tr>";
+
+    return sFila;
+}
+// ------------- FIN Clase GuardiaCivil hereda de PERSONA  -------------
+
+
+
+// ------------- Clase Multa  -------------
+
+class Multa {
+    constructor(idMulta, sNIFConductor, sNIFGuardia, fImporte, sDescripcion, dFecha) {
+        this.multa = idMulta;
+        this.NIFConductor = sNIFConductor;
+        this.NIFGuardia = sNIFGuardia;
+        this.importe = fImporte;
+        this.pagada = false;
+        this.descripcion = sDescripcion;
+        this.fecha = dFecha;
     }
 
     toHTMLRow() {
         let sFila = "<tr>";
-        sFila += "<td>" + this.NIF + "</td>";
-        sFila += "<td>" + this.nombre + "</td>";
-        sFila += "<td>" + this.apellidos + "</td>";
-        sFila += "<td>" + this.direccion + "</td>";
-        sFila += "<td>" + this.puesto + "</td>";
+        sFila += "<td>" + this.multa + "</td>";
+        sFila += "<td>" + this.NIFConductor + "</td>";
+        sFila += "<td>" + this.NIFGuardia + "</td>";
+        sFila += "<td>" + this.importe + "</td>";
+        sFila += "<td>" + this.pagada + "</td>";
+        sFila += "<td>" + this.descripcion + "</td>";
+        sFila += "<td>" + this.fecha + "</td>";
 
         sFila += "</tr>";
 
         return sFila;
     }
 
-}
-// ------------- FIN Clase GuardiaCivil hereda de PERSONA  -------------
+    _ListadoImporte() {
+        let sFila = "<tr>";
+        sFila += "<td>" + this.multa + "</td>";
+        sFila += "<td>" + `${this.fecha.getDate()}/${this.fecha.getMonth() + 1}/${this.fecha.getFullYear()}` + "</td>";
+        sFila += "<td>" + this.importe + "</td>";
+        sFila += "</tr>";
 
+        return sFila;
+    }
 
-
-
-
-// ------------- Clase Multa  -------------
-function Multa(idMulta, sNIFConductor, sNIFGuardia, fImporte, sDescripcion, dFecha) { //Clase Multa
-    this.multa = idMulta;
-    this.NIFConductor = sNIFConductor;
-    this.NIFGuardia = sNIFGuardia;
-    this.importe = fImporte;
-    this.pagada = false;
-    this.descripcion = sDescripcion;
-    this.fecha = dFecha;
 }
 
-Multa.prototype.toHTMLRow = function () { //Metodo de la clase Multa
-    let sFila = "<tr>";
-    sFila += "<td>" + this.multa + "</td>";
-    sFila += "<td>" + this.NIFConductor + "</td>";
-    sFila += "<td>" + this.NIFGuardia + "</td>";
-    sFila += "<td>" + this.importe + "</td>";
-    sFila += "<td>" + this.pagada + "</td>";
-    sFila += "<td>" + this.descripcion + "</td>";
-    sFila += "<td>" + this.fecha + "</td>";
-
-    sFila += "</tr>";
-
-    return sFila;
-}
-
-Multa.prototype._ListadoImporte = function () {
-    let sFila = "<tr>";
-    sFila += "<td>" + this.multa + "</td>";
-    sFila += "<td>" + `${this.fecha.getDate()}/${this.fecha.getMonth() + 1}/${this.fecha.getFullYear()}` + "</td>";
-    sFila += "<td>" + this.importe + "</td>";
-    sFila += "</tr>";
-
-    return sFila;
-}
 // ------------- FIN Clase Multa  -------------
 
 
 // ------------- Clase Leve hereda de MULTA  -------------
-function Leve(idMulta, sNIFConductor, sNIFGuardia, dImporte, sDescripcion, dFecha, bBonificada) { //Clase leve hereda de Multa
-    Multa.call(this, idMulta, sNIFConductor, sNIFGuardia, dImporte, sDescripcion, dFecha);
-    this.bonificada = bBonificada;
 
-};
+class Leve extends Multa {
+    constructor(idMulta, sNIFConductor, sNIFGuardia, dImporte, sDescripcion, dFecha, bBonificada) {
+        super(idMulta, sNIFConductor, sNIFGuardia, dImporte, sDescripcion, dFecha)
+        this.bonificada = bBonificada;
 
-Leve.prototype = Object.create(Multa.prototype);
-Leve.prototype.constructor = Leve;
+    }
 
-Leve.prototype.toHTMLRow = function () {
+    toHTMLRow() {
+        let sFila = "<tr>";
+        sFila += "<td>" + this.multa + "</td>";
+        sFila += "<td>" + this.NIFConductor + "</td>";
+        sFila += "<td>" + this.NIFGuardia + "</td>";
+        sFila += "<td>" + this.importe + "</td>";
+        sFila += "<td>" + this.pagada + "</td>";
+        sFila += "<td>" + this.descripcion + "</td>";
+        sFila += "<td>" + this.fecha + "</td>";
+        sFila += "<td>" + this.bonificada + "</td>";
 
-    let sFila = "<tr>";
-    sFila += "<td>" + this.multa + "</td>";
-    sFila += "<td>" + this.NIFConductor + "</td>";
-    sFila += "<td>" + this.NIFGuardia + "</td>";
-    sFila += "<td>" + this.importe + "</td>";
-    sFila += "<td>" + this.pagada + "</td>";
-    sFila += "<td>" + this.descripcion + "</td>";
-    sFila += "<td>" + this.fecha + "</td>";
-    sFila += "<td>" + this.bonificada + "</td>";
+        sFila += "</tr>";
 
-    sFila += "</tr>";
+        return sFila;
+    }
 
-    return sFila;
 
 }
+
 // ------------- FIN Clase Leve hereda de MULTA  -------------
 
 
-
 // ------------- Clase Grave hereda de MULTA  -------------
-function Grave(idMulta, sNIFConductor, sNIFGuardia, dImporte, sDescripcion, dFecha, iPuntos) {
-    Multa.call(this, idMulta, sNIFConductor, sNIFGuardia, dImporte, sDescripcion, dFecha);
-    this.puntos = iPuntos;
 
-};
+class Grave extends Multa {
+    constructor(idMulta, sNIFConductor, sNIFGuardia, dImporte, sDescripcion, dFecha, iPuntos) {
+        super(idMulta, sNIFConductor, sNIFGuardia, dImporte, sDescripcion, dFecha)
+        this.puntos = iPuntos;
+    }
 
-Grave.prototype = Object.create(Multa.prototype);
-Grave.prototype.constructor = Grave;
+    toHTMLRow() {
+        let sFila = "<tr>";
+        sFila += "<td>" + this.multa + "</td>";
+        sFila += "<td>" + this.NIFConductor + "</td>";
+        sFila += "<td>" + this.NIFGuardia + "</td>";
+        sFila += "<td>" + this.importe + "</td>";
+        sFila += "<td>" + this.pagada + "</td>";
+        sFila += "<td>" + this.descripcion + "</td>";
+        sFila += "<td>" + this.fecha + "</td>";
+        sFila += "<td>" + this.puntos + "</td>";
 
-Grave.prototype.toHTMLRow = function () {
+        sFila += "</tr>";
 
-    let sFila = "<tr>";
-    sFila += "<td>" + this.multa + "</td>";
-    sFila += "<td>" + this.NIFConductor + "</td>";
-    sFila += "<td>" + this.NIFGuardia + "</td>";
-    sFila += "<td>" + this.importe + "</td>";
-    sFila += "<td>" + this.pagada + "</td>";
-    sFila += "<td>" + this.descripcion + "</td>";
-    sFila += "<td>" + this.fecha + "</td>";
-    sFila += "<td>" + this.puntos + "</td>";
-
-    sFila += "</tr>";
-
-    return sFila;
-
+        return sFila;
+    }
 }
+
 // ------------- FIN Clase Grave hereda de MULTA  -------------
